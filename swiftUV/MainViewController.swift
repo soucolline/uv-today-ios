6b9fbf89b54b0fe6b2c0c6585c83ee6212e393e6
Thomas Guilleminot <guilleminot.thomas@gmail.com>
2017-03-15 11:59:42 +0100

    feat(develop): add basic layout

---
+++ swiftUV/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  swiftUV
+//
+//  Created by Thomas Guilleminot on 15/03/2017.
+//  Copyright © 2017 Thomas Guilleminot. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
