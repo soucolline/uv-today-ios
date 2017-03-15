7d404005108e8899201a2e990c869741204df196
Thomas Guilleminot <guilleminot.thomas@gmail.com>
2017-03-15 17:17:50 +0100

    feat(jsonFetch): all texts are white now

--- swiftUV/MainViewController.swift
+++ swiftUV/MainViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class MainViewController: UIViewController {
   
+  @IBOutlet weak var cityLabel: UILabel!
+  @IBOutlet weak var indexLabel: UILabel!
+  @IBOutlet weak var descriptionTextView: UITextView!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    // Add default backgroundColor
     self.view.backgroundColor = UIColor.colorFromInteger(color: UIColor.colorFromIndex(index: 2))
+    // Put all texts in white
+    self.cityLabel.textColor = UIColor.white
+    self.indexLabel.textColor = UIColor.white
+    self.descriptionTextView.textColor = UIColor.white
   }
   
 }
