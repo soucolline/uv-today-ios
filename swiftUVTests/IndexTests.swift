748144e2496b36acf33e6a305cfcd7fc5fa86c8f
Thomas Guilleminot <guilleminot.thomas@gmail.com>
2019-09-06 21:35:59 +0200

    feat(ios13): add support for dark mode

--- swiftUVTests/IndexTests.swift
+++ swiftUVTests/IndexTests.swift
@@ -16,13 +16,13 @@ class IndexTests: XCTestCase {
   func testGetColorFromIndex() {
     for index in 0...15 {
       switch index {
-      case 0: expect(index.associatedColor) == #colorLiteral(red: 0.2039215686, green: 0.5960784314, blue: 0.8588235294, alpha: 1)
-      case 1, 2: expect(index.associatedColor) == #colorLiteral(red: 0.1803921569, green: 0.8, blue: 0.4431372549, alpha: 1)
-      case 3, 4, 5: expect(index.associatedColor) == #colorLiteral(red: 0.9450980392, green: 0.768627451, blue: 0.05882352941, alpha: 1)
-      case 6, 7: expect(index.associatedColor) == #colorLiteral(red: 0.9058823529, green: 0.2980392157, blue: 0.2352941176, alpha: 1)
-      case 8, 9, 10: expect(index.associatedColor) == #colorLiteral(red: 0.7529411765, green: 0.2235294118, blue: 0.168627451, alpha: 1)
-      case 11, 12, 13, 14: expect(index.associatedColor) == #colorLiteral(red: 0.6078431373, green: 0.3490196078, blue: 0.7137254902, alpha: 1)
-      default : expect(index.associatedColor) == #colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+      case 0: expect(index.associatedColor) == UIColor.systemBlue
+      case 1, 2: expect(index.associatedColor) == UIColor.systemGreen
+      case 3, 4, 5: expect(index.associatedColor) == UIColor.systemYellow
+      case 6, 7: expect(index.associatedColor) == UIColor(named: "LightRed")!
+      case 8, 9, 10: expect(index.associatedColor) == UIColor.systemRed
+      case 11, 12, 13, 14: expect(index.associatedColor) == UIColor.systemPurple
+      default : expect(index.associatedColor) == UIColor.black
       }
     }
   }
