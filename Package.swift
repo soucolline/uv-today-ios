50869829b30f88848736b2bc711cda61cfec403d
Thomas Guilleminot <guilleminot.thomas@gmail.com>
2022-10-18 12:01:43 +0200

    feat(reducer-protocol): update TCA to 0.43.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
       .library(name: "UVClient", targets: ["UVClient"])
     ],
     dependencies: [
-      .package(url: "https://github.com/pointfreeco/swift-composable-architecture", exact: "0.42.0"),
+      .package(url: "https://github.com/pointfreeco/swift-composable-architecture", exact: "0.43.0"),
       .package(url: "https://github.com/pointfreeco/composable-core-location", exact: "0.2.0"),
     ],
     targets: [
