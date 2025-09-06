b84d4a5190e49da9c635e006cb9d7da23eb81640
Thomas Guilleminot <guilleminot.thomas@gmail.com>
2025-09-06 17:46:09 +0200

    feat(ios26): update reload button to be in toolbar for better glass effect

--- Sources/AppFeature/UVScreen.swift
+++ Sources/AppFeature/UVScreen.swift
@@ -20,16 +20,91 @@ public struct UVScreen: View {
   
   public var body: some View {
     WithPerceptionTracking {
-      ZStack {
-        Rectangle()
-          .animation(.easeIn(duration: 0.5), value: viewModel.uvIndex.associatedColor)
-          .foregroundColor(Color(viewModel.uvIndex.associatedColor))
-          .edgesIgnoringSafeArea(.all)
-
-        VStack {
-          HStack {
+      NavigationStack {
+        ZStack {
+          Rectangle()
+            .animation(.easeIn(duration: 0.5), value: viewModel.uvIndex.associatedColor)
+            .foregroundColor(Color(viewModel.uvIndex.associatedColor))
+            .edgesIgnoringSafeArea(.all)
+          
+          VStack {
+            HStack {
+              Spacer()
+            }
+            
+            HStack {
+              Text(viewModel.cityName)
+                .padding(.top, 12)
+                .font(.system(size: 38, weight: .bold, design: .rounded))
+                .foregroundColor(.white)
+                .padding(.horizontal, 20)
+                .lineLimit(1)
+                .minimumScaleFactor(0.2)
+                .redacted(reason: viewModel.getCityNameRequestInFlight ? .placeholder : [])
+              Spacer()
+            }
+            
             Spacer()
             
+            Text(String(viewModel.uvIndex))
+              .foregroundColor(.white)
+              .font(.system(size: 80, weight: .semibold, design: .rounded))
+              .redacted(reason: viewModel.weatherRequestInFlight ? .placeholder : [])
+            
+            Spacer()
+            
+            if viewModel.isLocationRefused {
+              Button {
+                UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+              } label: {
+                Label("app.error.openSettings".localized, systemImage: "location")
+              }
+              .foregroundColor(.black)
+              .padding(20)
+              .background(Color.white)
+              .cornerRadius(8)
+            }
+            
+            Text(viewModel.uvIndex.associatedDescription)
+              .padding(20)
+              .foregroundColor(.white)
+              .font(.system(size: 12))
+              .redacted(reason: viewModel.weatherRequestInFlight ? .placeholder : [])
+            
+            if let attributionLogo = viewModel.attributionLogo {
+              AsyncImage(url: attributionLogo, content: { image in
+                image
+                  .resizable()
+                  .scaledToFit()
+                  .frame(maxWidth: 100)
+              }, placeholder: {
+                
+              })
+            }
+            
+            if let attributionLink = viewModel.attributionLink {
+              Link(destination: attributionLink, label: { Text("Other data sources")})
+                .foregroundColor(.white)
+            }
+          }
+        }
+        .alert(isPresented: $viewModel.shouldShowErrorPopup) {
+          Alert(title: Text("app.label.error"), message: Text(viewModel.errorText))
+        }
+        .onChange(of: scenePhase) { value in
+          switch value {
+          case .active: viewModel.onAppear()
+          case .inactive, .background: viewModel.onDisappear()
+          @unknown default: break
+          }
+        }
+        .task {
+          if #available(iOS 16.0, *) {
+            await viewModel.getAtribution()
+          }
+        }
+        .toolbar {
+          ToolbarItem {
             Button {
               Task {
                 await viewModel.getUVRequest()
@@ -38,84 +113,12 @@ public struct UVScreen: View {
               Image(systemName: "arrow.clockwise")
                 .resizable()
                 .frame(width: 20, height: 20, alignment: .center)
-                .font(Font.title.weight(Font.Weight.thin))
-                .foregroundColor(.white)
+                .font(Font.title.weight(Font.Weight.bold))
+                .toolbarColor()
                 .padding()
-                .glassEffectCustom()
-                .padding(.trailing, 20)
             }
             .disabled(viewModel.isLocationRefused)
           }
-
-          HStack {
-            Text(viewModel.cityName)
-              .padding(.top, 12)
-              .font(.system(size: 38, weight: .bold, design: .rounded))
-              .foregroundColor(.white)
-              .padding(.horizontal, 20)
-              .lineLimit(1)
-              .minimumScaleFactor(0.2)
-              .redacted(reason: viewModel.getCityNameRequestInFlight ? .placeholder : [])
-            Spacer()
-          }
-
-          Spacer()
-
-          Text(String(viewModel.uvIndex))
-            .foregroundColor(.white)
-            .font(.system(size: 80, weight: .semibold, design: .rounded))
-            .redacted(reason: viewModel.weatherRequestInFlight ? .placeholder : [])
-
-          Spacer()
-          
-          if viewModel.isLocationRefused {
-            Button {
-              UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
-            } label: {
-              Label("app.error.openSettings".localized, systemImage: "location")
-            }
-            .foregroundColor(.black)
-            .padding(20)
-            .background(Color.white)
-            .cornerRadius(8)
-          }
-          
-          Text(viewModel.uvIndex.associatedDescription)
-            .padding(20)
-            .foregroundColor(.white)
-            .font(.system(size: 12))
-            .redacted(reason: viewModel.weatherRequestInFlight ? .placeholder : [])
-          
-          if let attributionLogo = viewModel.attributionLogo {
-            AsyncImage(url: attributionLogo, content: { image in
-              image
-                .resizable()
-                .scaledToFit()
-                .frame(maxWidth: 100)
-            }, placeholder: {
-              
-            })
-          }
-          
-          if let attributionLink = viewModel.attributionLink {
-            Link(destination: attributionLink, label: { Text("Other data sources")})
-              .foregroundColor(.white)
-          }
-        }
-      }
-      .alert(isPresented: $viewModel.shouldShowErrorPopup) {
-        Alert(title: Text("app.label.error"), message: Text(viewModel.errorText))
-      }
-      .onChange(of: scenePhase) { value in
-        switch value {
-        case .active: viewModel.onAppear()
-        case .inactive, .background: viewModel.onDisappear()
-        @unknown default: break
-        }
-      }
-      .task {
-        if #available(iOS 16.0, *) {
-          await viewModel.getAtribution()
         }
       }
     }
@@ -128,9 +131,9 @@ public struct UVScreen: View {
 
 extension View {
   @ViewBuilder
-  func glassEffectCustom() -> some View {
-    if #available(iOS 26, *) {
-      glassEffect()
+  func toolbarColor() -> some View {
+    if #unavailable(iOS 26) {
+      foregroundColor(.white)
     } else {
       self
     }
